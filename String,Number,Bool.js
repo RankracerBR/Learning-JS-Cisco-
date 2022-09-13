ee3ecf8151621acfcb2e2813729c4aa691f02684
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-09-13 14:25:39 -0300

    Create String,Number,Bool.js

---
+++ String,Number,Bool.js
@@ -0,0 +1,17 @@
+// String (will create a simple string).
+// Number(will create a value and return 0).
+// Boolean (will create and retunr the value of false).
+// BigInt(Command that requires a initial value to it).
+
+const str = String(); 
+const num = Number();
+const bool = Boolean(); 
+
+console.log(str); 
+console.log(num);
+console.log(bool);
+
+const big1 = BigInt(42); 
+console.log(big1);
+
+const big2 = BigInt();
