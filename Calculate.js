3f780a397f67b36e43b111b54f19157500642744
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2022-10-03 23:27:41 -0300

    Create Calculate.js

---
+++ Calculate.js
@@ -0,0 +1,15 @@
+//3.1.1.4 
+
+console.log(2+2*2); 
+console.log(2+ (2*2)); 
+console.log((2+2)*2);
+
+const x = 5;
+const y = 2; 
+
+console.log("addition: ", x+y); 
+console.log("subtraction: ", x-y); 
+console.log("multiplication: ", x*y); 
+console.log("division: ", x/y);
+console.log("division remainder: ",x % y ); 
+console.log("exponent:", x**y);
