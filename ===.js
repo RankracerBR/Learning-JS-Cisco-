7b8258398483b0493763988cdb69fc0b4cbd7205
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2022-10-03 23:34:14 -0300

    Create ===.js

---
+++ ===.js
@@ -0,0 +1,38 @@
+//3.2.1.3 
+
+console.log(10 === 5); 
+console.log(10 === 10);
+console.log(10 === 10n); 
+console.log(10 === "10");
+console.log("10" === "10"); 
+console.log("Alice" === "Bob");
+console.log(0 === false); 
+console.log(undefined === false);
+////////////////////////////////////////
+console.log(10 == 5); 
+console.log(10 == 10);
+console.log(10 == 10n); 
+console.log(10 == "10");
+console.log("10" == "10" ); 
+console.log("Alice" == "Bob");
+console.log(0 == false); 
+console.log(undefined == false); 
+console.log(NaN == NaN);
+//////////////////////////////////////// !== not identical returns = true  != returns true when the operands are different.
+console.log(10 !== 5); 
+console.log(10 !== 10);
+console.log(10 !== 10n); 
+console.log("10" !== "10");
+console.log("Alice" !== "Bob"); 
+console.log(0 !== false);
+console.log(undefined !== false); 
+console.log(10 != 5);
+console.log(10 !=10); 
+console.log(10 != 10n);
+console.log(10 != "10"); 
+console.log("10" != "10");
+console.log("Alice" != "Bob"); 
+console.log(0 != false);
+console.log(undefined != false); 
+console.log(NaN != NaN);
+ 
