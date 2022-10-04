cab81570bd48020d6b210c01c7a16f63e5c6ee2c
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2022-10-03 23:34:02 -0300

    Create TypeOf_2.js

---
+++ TypeOf_2.js
@@ -0,0 +1,13 @@
+// 3.1.1.5 
+
+let str = "123";
+let n1 = +str;
+let n2 = -str;
+let n3 = -n2;
+let n4 = +"abcd"; 
+
+console.log(str,typeof str); 
+console.log(n1,typeof n1); 
+console.log(n2, typeof n2); 
+console.log(n3, typeof n3); 
+console.log(n4, typeof n4);
