0ab614fc279a586b5421324f7b65804b4e9c42ea
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2022-10-03 23:34:08 -0300

    Create Calculate_2.js

---
+++ Calculate_2.js
@@ -0,0 +1,16 @@
+// 3.1.1.7
+
+let x = 10; 
+// The values are changed in each count 
+x += 2;
+console.log(x); // 10 + 2 = 12 
+x -= 4;
+console.log(x);// 12- 4 = 8  
+x *= 3; 
+console.log(x); // 8 * 4 = 24
+x /= 6; 
+console.log(x); // 24 / 6 = 4
+x **= 3;
+console.log(x); // 4 * 4 * 4 = 64
+x %= 10;
+console.log(x); // The rest is = 4 
