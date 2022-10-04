44da6eef56cca254bdd95274528002ae09b22814
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2022-10-03 23:34:00 -0300

    Create Tasks.js

---
+++ Tasks.js
@@ -0,0 +1,22 @@
+// 3.1.1.12 
+
+//task 1 
+
+console.log(2*3+1); 
+console.log(2^4); 
+console.log(5*1); 
+console.log(8 ** 2 - 5 ** 2);
+
+//task 2
+console.log(4 * 5 === 20); 
+console.log(6 * 5 == "30"); 
+console.log(-17 < 0); 
+console.log(25 > 1); 
+console.log(2+ 2 * 2 != 4); 
+
+//taks 3 
+
+console.log(true || false); 
+console.log(false || ! false); 
+console.log(false || false || true); 
+console.log(true || false && false && true);
